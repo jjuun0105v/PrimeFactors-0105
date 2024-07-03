169c7be26d39cf2d6ce6f460ecbef34f9516e7e8
juwon.kim <saylily90@naver.com>
2024-07-03 10:51:50 +0900

    push project

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "../Project8/PrimeFactor.cpp"
+
+class PrimeFactorFixture : public testing::Test
+{
+public:
+	PrimeFactor primeFactor;
+
+};
+
+TEST_F(PrimeFactorFixture, tc) {
+
+
+}
